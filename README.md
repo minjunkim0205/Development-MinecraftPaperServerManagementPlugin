923b3ae53eba8dd93d5d8523817cb6e0e5c1637a
minjunkim0205 <minjunkim0205@gmail.com>
2025-09-18 14:34:24 +0900

    Update README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+# 💻 Development-MinecraftPaperServerManagementPlugin  
+
+## Preview  
+
+![Preview](./Preview.png)  
+
+- ## Introduction  
+
+    > Minecraft Java 버전의 Paper 버킷용 서버 메니지먼트 플러그인 개발  
+
+- ## Requirements  
+
+    > JDK 21  
+    > Gradle  
+
+- ## Note  
+
+    > 빌드 하는법  
+    > .\gradlew build  
--- src/main/java/org/plugin/CorePlugin.java
+++ src/main/java/org/plugin/CorePlugin.java
@@ -5,11 +5,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class CorePlugin extends JavaPlugin {
     @Override
     public void onEnable() {
-        getLogger().info("플러그인 시작 테스트");
+        getLogger().info("Start");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("플러그인 종료 테스트");
+        getLogger().info("Stop");
     }
 }
